5692b1ef405b507190d05c7616037845b8b92229
keizien <bergeaudleo@gmail.com-github@users.noreply.github.com>
2025-11-17 17:59:42 +0100

    AP2 cybersecu

---
+++ migrations/Version20251117164721.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20251117164721 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user ADD lastname VARCHAR(100) NOT NULL, ADD firstname VARCHAR(100) NOT NULL, ADD adresse VARCHAR(100) NOT NULL, ADD ville VARCHAR(100) NOT NULL, ADD code_postal INT NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user DROP lastname, DROP firstname, DROP adresse, DROP ville, DROP code_postal');
+    }
+}
