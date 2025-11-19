0431a66187ed61b146d41d457c90a6846aaa576f
mevyona <mevyona@gmail.com>
2025-11-19 14:23:28 +0100

    Liste des users

---
+++ src/Controller/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\UserRepository;
+
+final class UserController extends AbstractController
+{
+    #[Route('/list-user', name: 'app_userList')]
+    public function index(UserRepository $userRepository): Response
+    {
+        $users = $userRepository->findAll();
+        return $this->render('user/index.html.twig', [
+            'users' => $users
+        ]);
+    }
+}
